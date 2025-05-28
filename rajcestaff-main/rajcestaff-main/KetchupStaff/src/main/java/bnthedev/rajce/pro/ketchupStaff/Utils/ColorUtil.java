538afd9d05679ab2e30866c269af108606983e08
bbnthedev <benjarehak@gmail.com>
2025-05-28 12:11:08 +0200

    Delete rajcestaff-main/rajcestaff-main/KetchupStaff directory

--- rajcestaff-main/rajcestaff-main/KetchupStaff/src/main/java/bnthedev/rajce/pro/ketchupStaff/Utils/ColorUtil.java
+++
@@ -1,9 +0,0 @@
-package bnthedev.rajce.pro.ketchupStaff.Utils;
-
-import org.bukkit.ChatColor;
-
-public class ColorUtil {
-    public static String color(String msg) {
-        return ChatColor.translateAlternateColorCodes('&', msg);
-    }
-}
